ad6cf8ba81aa653905686de65d4715a9a659ae7a
Daniel Cobej <danielcobej@gmail.com>
2024-04-23 00:27:27 +0200

    fix

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@
 
 apt update
 apt install -y python3 python3-pip python3-dev python3-venv htop iftop mc wget
+mkdir monailabel
+cd monailabel
+python3 -m venv .venv
+source .venv/bin/activate
 pip install monailabel
 
