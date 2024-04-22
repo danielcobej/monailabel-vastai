aad56d3c4e2e454994e9203beb1ad4b21b83e97b
Daniel Cobej <danielcobej@gmail.com>
2024-04-23 00:25:24 +0200

    Fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt update
-apt install -y python3 python3-pip python3-dev python3-venv htop iftop mc nvtop wget gh
+apt install -y python3 python3-pip python3-dev python3-venv htop iftop mc wget
 pip install monailabel
 
