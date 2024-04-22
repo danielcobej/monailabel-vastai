99f2e3839da4b255d04b6aefb3cbdbc82d6f7e64
Daniel Čobej <72991416+danielcobej@users.noreply.github.com>
2024-04-23 01:17:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ apt update
 apt install -y python3 python3-pip python3-dev python3-venv htop iftop mc wget unzip
 mkdir monailabel
 cd monailabel
-python3 -m venv .venv
-source .venv/bin/activate
 pip install monailabel
 pip install dicomweb-client[gcp]
 wget http://128.140.41.93:8888/amos.zip
